6d34d414e6d40120f9326b5e74b5d5a206b35f08
BDEMARCHE <brunodemarche@hotmail.com>
2024-06-27 12:49:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 terraform {
   required_version = ">= 1.6.0, < 2.0.0"
 
-  cloud {
-    organization = "M2I"
-    workspaces {
-      name = "terraform"
-    }
-  }
+
 
   required_providers {
     azurerm = {
